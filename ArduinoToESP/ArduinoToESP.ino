61c3d77f090f30dc74e4d438538d078f79c4dac9
paulpenney <paul.penney@gmail.com>
2023-09-25 21:07:25 -0400

    Updated

---
+++ ArduinoToESP/ArduinoToESP.ino
@@ -0,0 +1,45 @@
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+#include <SoftwareSerial.h>
+
+SoftwareSerial mySerial(10, 11);  // RX, TX
+LiquidCrystal_I2C lcd(0x27, 16, 2);  // LCD address, 16 column and 2 rows
+unsigned long lastLogTime = 0;
+String receivedData = "";
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Starting Arduino!");
+  mySerial.begin(9600);
+  lcd.begin(16, 2);
+  lcd.backlight();
+  lcd.clear();
+  lcd.print("Starting Arduino!");
+  pinMode(13, OUTPUT);
+}
+
+void loop() {
+  while (mySerial.available()) {
+    char c = mySerial.read();
+    if (c == '\n') {
+      Serial.println("Received: " + receivedData);
+      lcd.clear();
+      lcd.setCursor(0, 0);
+      lcd.print("Received:");
+      lcd.setCursor(0, 1);
+      lcd.print(receivedData);
+      mySerial.println("Message received by Arduino");
+      receivedData = "";
+    } else {
+      receivedData += c;
+    }
+  }
+
+  unsigned long currentTime = millis();
+  if (currentTime - lastLogTime >= 1000) {
+    lastLogTime = currentTime;
+    digitalWrite(13, HIGH);
+    delay(200);
+    digitalWrite(13, LOW);
+  }
+}
---
+++ ESPtoArduino/ESPtoArduino.ino
@@ -0,0 +1,46 @@
+#include <SoftwareSerial.h>
+
+SoftwareSerial mySerial(D5, D6);  // RX, TX
+String receivedData = "";
+unsigned long lastMessageTime = 0;
+unsigned long lastBlinkTime = 0;
+
+void setup() {
+  Serial.begin(9600);
+  mySerial.begin(9600);
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop() {
+  unsigned long currentTime = millis();
+
+  // Send message every second
+  if (currentTime - lastMessageTime >= 300) {
+    lastMessageTime = currentTime;
+    String message = "Hello " + String(currentTime);
+    mySerial.print(message + '\n');
+  }
+
+  // Read incoming messages
+  while (mySerial.available()) {
+    char c = mySerial.read();
+    if (c == '\n') {
+      Serial.println("Received: " + receivedData);
+      receivedData = "";
+    } else {
+      receivedData += c;
+    }
+  }
+
+  // Strobe LED twice every second (100ms per strobe)
+  if (currentTime - lastBlinkTime >= 1000) {
+    digitalWrite(LED_BUILTIN, LOW);
+  }
+  if (currentTime - lastBlinkTime >= 1200) {
+    digitalWrite(LED_BUILTIN, HIGH);
+  }
+  if (currentTime - lastBlinkTime >= 1300) {
+    digitalWrite(LED_BUILTIN, LOW);
+    lastBlinkTime = currentTime;
+  }
+}
